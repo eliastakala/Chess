3d76c2a251c06488a0e3fe59548ed804ae3ad24a
eliastakala <etakala1998@gmail.com>
2026-06-20 10:37:44 +0300

    feat/clicking a square works

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import "./App.css";
 
-const Square= ({ rank, file }) => {
+const Square= ({ rank, file, handleClick }) => {
   const isLight = (rank + file) % 2 === 0;
   return (
     <div
       className={`square ${isLight ? "light" : "dark"}`}
+      onClick={handleClick}
     />
   );
 }
 
 const Board = () => {
   const indices = [0, 1, 2, 3, 4, 5, 6, 7];
+  const clickSquare = ({rank, file}) => {
+    console.log("clicked", rank, file)
+  }
 
   return (
     <div className="board">
@@ -20,6 +24,7 @@ const Board = () => {
             key={`${rank}-${file}`}
             rank={rank}
             file={file}
+            handleClick={() => clickSquare({rank: { rank }, file: { file }})}
           />
         ))
       )}
