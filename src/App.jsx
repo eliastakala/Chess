8fc46bf9f0fb5cf826df68746bd313213e7f5be0
eliastakala <etakala1998@gmail.com>
2026-06-20 11:22:11 +0300

    feat/gamestate started

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
-const Square= ({ rank, file, handleClick }) => {
+const pieces = ['p', 'r', 'kn', 'b', 'k', 'q', 'P', 'R', 'KN', 'B', 'K', 'Q', '']
+
+const initialState = [
+  ['1', '2', '3', '4', '5', '6', '7', '8'],
+  ['1', '2', '3', '4', '5', '6', '7', '8'],
+  ['1', '2', '3', '4', '5', '6', '7', '8'],
+  ['1', '2', '3', '4', '5', '6', '7', '8'],
+  ['1', '2', '3', '4', '5', '6', '7', '8'],
+  ['1', '2', '3', '4', '5', '6', '7', '8'],
+  ['1', '2', '3', '4', '5', '6', '7', '8'],
+  ['1', '2', '3', '4', '5', '6', '7', '8']
+]
+
+const Square= ({ rank, file, handleClick, piece }) => {
   const isLight = (rank + file) % 2 === 0;
   return (
     <div
       className={`square ${isLight ? "light" : "dark"}`}
       onClick={handleClick}
-    />
+    >{piece}</div>
   );
 }
 
-const Board = () => {
+const Board = ({ state }) => {
   const indices = [0, 1, 2, 3, 4, 5, 6, 7];
-  const clickSquare = ({rank, file}) => {
-    console.log("clicked", rank, file)
+  const clickSquare = ({rank, file, piece}) => {
+    console.log("clicked", rank, file, piece)
   }
 
   return (
@@ -24,7 +38,9 @@ const Board = () => {
             key={`${rank}-${file}`}
             rank={rank}
             file={file}
-            handleClick={() => clickSquare({rank: { rank }, file: { file }})}
+            
+            piece={state[rank][file]}
+            handleClick={() => clickSquare({rank: { rank }, file: { file }, piece: {piece}})}
           />
         ))
       )}
@@ -32,9 +48,11 @@ const Board = () => {
   );
 }
 
-function App() {
+const App= () => {
+  const [gameState, setGameState] = useState(initialState)
+
   return (
-    <Board />
+    <Board state={gameState}/>
   );
 }
 
