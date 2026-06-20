bc2c625c7aae5b97f252a69d7f892ca1056245d7
eliastakala <etakala1998@gmail.com>
2026-06-20 10:21:08 +0300

    fix/better implementation for the board

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import './App.css'
+import "./App.css";
 
-function App() {
-
+const Square= ({ rank, file }) => {
+  const isLight = (rank + file) % 2 === 0;
   return (
-    <div>
-      <div className='node'></div>
-    </div>
-  )
+    <div
+      className={`square ${isLight ? "light" : "dark"}`}
+    />
+  );
 }
 
-export default App
+const Board = () => {
+  const indices = [0, 1, 2, 3, 4, 5, 6, 7];
+
+  return (
+    <div className="board">
+      {indices.map((rank) =>
+        indices.map((file) => (
+          <Square
+            key={`${rank}-${file}`}
+            rank={rank}
+            file={file}
+          />
+        ))
+      )}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Board />
+  );
+}
+
+export default App;
